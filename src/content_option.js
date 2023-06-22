46f825f5da799f2a0eb23cd43c5b09f5943d4a11
Danielle Hoey <97096664+danianise@users.noreply.github.com>
2023-06-22 09:55:27 -0400

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -17,7 +17,7 @@ const introdata = {
 
 const dataabout = {
     title: "about",
-    aboutme: "I am a Software Engineer Located in the Greater Boston area. My extensive customer service experience drives me to improve the well-being of others with an empathetic approach to empowerment. As a career-changer I am committed to continuously learning new technologies. I am currently seeking the next opportunity to grow my skillset with hands-on experience. I have a Bachelor of Business Administration from the Isenberg School of Management at the University of Massachusetts, Amherst, 15 years experience in retail customer service, and I have recently completed the Software Engineering Immersive course with General Assembly. Currently I'm working freelance as a Web Developer, building e-commerce websites for small businesses. I'm a tattoo enthusiast. I'm a nerd for fantasy and sci-fi. I collect hard-cover books. My favorite author is Margaret Atwood. I have a weird knack for finding four-leaf clovers and inside the larger books on my shelf you'll find at least a hundred of them waiting to be cast in resin. I'm a mom to kids and cats, and I hope to spend the rest of my life learning and experiencing."};
+    aboutme: ""};
 const worktimeline = [{
         jobtitle: "HTML",
         where: "",
@@ -80,7 +80,7 @@ const skills = [{
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Django",
         value: 85,
     },
     {
